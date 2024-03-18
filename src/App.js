6b16669759cda6b5838070ced0fac97a176db539
MonaBurger <mona@software-lab.de>
2024-03-18 22:49:06 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Mona Burger and is{" "}
+          <a
+            href="https://github.com/MonaBurger/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+          .
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
